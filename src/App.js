c7cefe878915f8f24a274327bd69fc50ba3df2d2
Aman Pratap Singh <56557379+Spider8019@users.noreply.github.com>
2023-04-05 00:56:35 +0530

    asdf

--- src/App.js
+++ src/App.js
@@ -9,45 +9,21 @@ import Navbar from './components/global/Navbar';
 function App() {
   const constraintsRef = useRef(null);
   const [visibleBottom, setVisibleBottom] = useState(false);
-  const [reqs, setReqs] = useState([{
-    title: "Break up with shweta",
-    date: new Date("2022-12-24"),
-    type: "+"
-    // clockColor:"red"
-  }, {
-    title: "Things gone wrong with shipra",
-    date: new Date("2023-04-01T22:00:00+05:30"),
-    type: "+"
-  }, {
-    title: "Masturbated Last Time",
-    date: new Date("2023-04-01T16:20:00+05:30"),
-    type: "+"
-  }, {
-    title: "Carbonated Drink's Break till",
-    date: new Date("2023-09-21"),
-    type: "-"
-  }, {
-    title: "New year 2024",
-    date: new Date("2024-01-01"),
-    type: "-"
-  }, {
-    title: "College life will end in",
-    date: new Date("2023-05-10"),
-    type: "-"
-  }])
+  const [reqs, setReqs] = useState([])
   useEffect(() => {
     allTask().then(data => { console.log(data); setReqs(data) })
       .catch(err => console.log(err))
   }, [])
 
-  // if (reqs.length === 0)
-  //   return <p className='p-2 text-center text-white-500'>Loading...</p>
+  if (reqs.length === 0)
+    return <p className='p-2 text-center text-white-500'>Loading...</p>
+
   return (
     <React.Fragment>
       <Navbar
         visibleBottom={visibleBottom}
         setVisibleBottom={setVisibleBottom} />
-      <div className="App mt-2"
+      <div className="App mt-2 px-2"
         ref={constraintsRef}
       >
         <FormInput
