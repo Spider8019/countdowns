b0fdb9ff30fa5b0333c5d9f9847581c2e8d50eb1
Aman Pratap Singh <56557379+Spider8019@users.noreply.github.com>
2023-04-18 03:51:53 +0530

    adfa

--- src/store.js
+++ src/store.js
@@ -1,34 +1,35 @@
 import { configureStore } from "@reduxjs/toolkit";
 import global from "./redux/slices/global";
-import storageSession from "redux-persist/lib/storage";
-import { persistReducer, persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER, } from 'redux-persist';
+// import storageSession from "redux-persist/lib/storage";
+// import { persistReducer, persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER, } from 'redux-persist';
 import { combineReducers } from "redux"
 
-const persistConfig = {
-    key: 'root',
-    storage:storageSession,
-    version: 1,
-}
+// const persistConfig = {
+//     key: 'root',
+//     storage:storageSession,
+//     version: 1,
+// }
 
 
 const reducers = combineReducers({
     global,
 });
 
-const persistedReducer = persistReducer(persistConfig, reducers);
+// const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-    reducer: persistedReducer,
-    devTools: process.env.NODE_ENV !== 'production',
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-            },
-        }),
+    reducer:reducers
+    // reducer: persistedReducer,
+    // devTools: process.env.NODE_ENV !== 'production',
+    // middleware: (getDefaultMiddleware) =>
+    //     getDefaultMiddleware({
+    //         serializableCheck: {
+    //             ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    //         },
+    //     }),
 
 })
 
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);
 
 
